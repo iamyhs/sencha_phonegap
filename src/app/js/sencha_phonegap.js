af3e4fd214f9b36782b0d5a79322823940b74b47
iamyhs <iamyhs@gmail.com>
2011-12-22 14:14:28 +1000

    addde phonegap feature lists

--- src/app/js/sencha_phonegap.js
+++ src/app/js/sencha_phonegap.js
@@ -330,14 +330,66 @@ var App = new Ext.Application({
         	}]
         });
         
-        App.views.phonegapFeature = new Ext.form.FormPanel({
-        	id:'phonegapFeature',
-        	
-        	loadRecord:function(record){
-        		Ext.Msg.alert('Record Info', record.data.id+" : "+record.data.name, Ext.emptyFn);
-        	}
-        	
-        });
+        
+        
+        // App.views.phonegapFeature = new Ext.form.FormPanel({
+//         	
+        	// id:'phonegapFeature',        	
+        	// loadRecord:function(record){
+        		// //Ext.Msg.alert('Record Info', record.data.id+" : "+record.data.name, Ext.emptyFn);
+        		// switch(record.data.id){
+//         			
+        			// case 1:
+        				// //Ext.Msg.alert('Record Info', record.data.name, Ext.emptyFn);
+        				// //alert(this.id);
+        				// //App.views.viewport.setActiveItem('accelerometer', { type: 'slide', direction: 'left' });
+						// //items:[App.views.Accelerometer];	
+						// //this.items = [App.views.Accelerometer];		
+        			// break;
+//         			
+        			// case 2:
+        			// break;
+//         			
+        			// case 3:
+        			// break;
+//         			
+        			// case 4:
+        			// break;
+//         			
+        			// case 5:
+        			// break;
+//         			
+        			// case 6:
+        			// break;
+//         			
+        			// case 7:
+        			// break;
+//         			
+        			// case 8:
+        			// break;
+//         			
+        			// case 9:
+        			// break;
+//         			
+        			// case 10:
+        			// break;
+//         			
+        			// case 11:
+        			// break;
+//         			
+        			// case 12:
+        			// break;
+//         			
+        			// case 13:
+        			// break;
+//         			
+        			// default:
+        			// //App.views.viewport.setActiveItem('phonegapMain', { type: 'slide', direction: 'left' });
+//         			
+        		// }
+        	// }
+//         	
+        // });
                 
 
         App.views.phonegapFeaturesListToolbar = new Ext.Toolbar({
@@ -347,7 +399,7 @@ var App = new Ext.Application({
                     text: 'Home',
                     ui: 'back',
                     handler: function () {
-                        App.views.viewport.setActiveItem('phonegapMain', { type: 'slide', direction: 'left' });
+                        App.views.viewport.setActiveItem('phonegapMain', { type: 'slide', direction: 'left' });                        
                     }
                 },
                 { xtype: 'spacer' }
@@ -361,18 +413,278 @@ var App = new Ext.Application({
         	itemTpl: '<div class="list-item-name">{name}</div>',
             
         	onItemDisclosure:function(record){
-        		App.views.phonegapFeature.load(record);
+				//App.views.phonegapFeature.load(record);        		
         		App.views.viewport.setActiveItem('phonegapFeaturesListContainer', { type: 'slide', direction: 'right' });
+        		App.views.phonegapFeaturesListContainer.showPhoneGapView(record);
+        		//App.views.viewport.setActiveItem('accelerometer', { type: 'slide', direction: 'left' });
         	}        	
         });
+
+		App.views.Accelerometer = new Ext.Container({
+        	id:'accelerometer',
+        	fullscreen:true,
+        	//html:'Accelerometer Page'
+        	items:[
+        	{
+        		xtype:'button'
+        	},{
+        		xtype:'button'	
+        	}
+        	]
+        });
         
+        
+        App.views.Camera = new Ext.Container({
+        	id:'camera',
+        	fullscreen:true,
+        	html:'Camera Page'        	
+        	//items:[{xtype:'button'}]
+        });
+
+        App.views.Capture = new Ext.Container({
+        	id:'capture',
+        	fullscreen:true,
+        	html:'Capture Page'        	
+        	//items:[{xtype:'button'}]
+        });
+
+
+        App.views.Compass = new Ext.Container({
+        	id:'compass',
+        	fullscreen:true,
+        	html:'Compass Page'        	
+        	//items:[{xtype:'button'}]
+        });
+
+        App.views.Connection = new Ext.Container({
+        	id:'connection',
+        	fullscreen:true,
+        	html:'Connection Page'        	
+        	//items:[{xtype:'button'}]
+        });        
+
+
+        App.views.Contacts = new Ext.Container({
+        	id:'contacts',
+        	fullscreen:true,
+        	html:'Contacts Page'        	
+        	//items:[{xtype:'button'}]
+        });
+
+
+        App.views.Device = new Ext.Container({
+        	id:'device',
+        	fullscreen:true,
+        	html:'Device Page'        	
+        	//items:[{xtype:'button'}]
+        });
+
+        App.views.Events = new Ext.Container({
+        	id:'events',
+        	fullscreen:true,
+        	html:'Events Page'        	
+        	//items:[{xtype:'button'}]
+        });        
+
+        App.views.File = new Ext.Container({
+        	id:'file',
+        	fullscreen:true,
+        	html:'File Page'        	
+        	//items:[{xtype:'button'}]
+        });
+
+
+        App.views.Geolocation = new Ext.Container({
+        	id:'geolocation',
+        	fullscreen:true,
+        	html:'Geolocation Page'        	
+        	//items:[{xtype:'button'}]
+        });
+
+
+        App.views.Media = new Ext.Container({
+        	id:'media',
+        	fullscreen:true,
+        	html:'Media Page'        	
+        	//items:[{xtype:'button'}]
+        });
+
+        App.views.Notification = new Ext.Container({
+        	id:'notification',
+        	fullscreen:true,
+        	html:'Notification Page'        	
+        	//items:[{xtype:'button'}]
+        });
+
+        App.views.Storage = new Ext.Container({
+        	id:'storage',
+        	fullscreen:true,
+        	html:'Storage Page'        	
+        	//items:[{xtype:'button'}]
+        });
+                                                            
+        //Custom component contain Ext.Panel and Data Process or custom handler 
         App.views.phonegapFeaturesListContainer = new Ext.Panel({
             id: 'phonegapFeaturesListContainer',
             layout: 'fit',
             dockedItems: [App.views.phonegapFeaturesListToolbar],
-            items: [App.views.phonegapFeature]
+            items:[],
+            // listeners: {
+		        // added: addedHandler,
+		        // scope: this
+		    // },
+            
+            // initComponent:function(){
+//             	
+            	// console.log(this.id);
+            	// //App.views.phonegapFeaturesListContainer.superclass.initComponent.apply(this, arguments);
+            	// //this.add(App.views.Accelerometer);
+            // },
+            swapArray:function (array,x) {
+			  var b = array[x];
+			  array[x] = array[array.length-1];
+			  array[array.length-1] = b;
+			  
+			  var a = this.items.keys[x];
+			  this.items.keys[x] = this.items.keys[this.items.keys.length-1];
+			  this.items.keys[this.items.keys.length-1] = a;
+			  
+			  return this;
+			},
+			
+			clickedItemShow:function(name){
+				//this.items.items.each(function(item){
+				this.items.each(function(item){					
+					if(item.id == name){
+						item.show();
+					}else{
+						item.hide();
+					}
+				});
+			},
+
+            isExist:function(targetArray, string){
+            	if(targetArray.indexOf(string) == -1){
+            		return false;
+            	}
+            	return true;
+            },
+            
+            showPhoneGapView:function(record){            	
+            	App.views.phonegapFeaturesListToolbar.setTitle(record.data.name);
+            	switch(record.data.id){
+        			
+        			case 1:
+						if(!this.isExist(this.items.keys, "accelerometer")){
+							this.add("accelerometer");							
+						}else{
+							this.clickedItemShow("accelerometer");
+						}
+        			break;
+        			
+        			case 2:
+        				if(!this.isExist(this.items.keys, "camera")){
+							this.add("camera");							
+						}else{
+							this.clickedItemShow("camera");
+						}
+        			break;
+        			
+        			case 3:
+        				if(!this.isExist(this.items.keys, "capture")){
+							this.add("capture");							
+						}else{
+							this.clickedItemShow("capture");
+						}
+        			break;
+        			
+        			case 4:
+        				if(!this.isExist(this.items.keys, "compass")){
+							this.add("compass");							
+						}else{
+							this.clickedItemShow("compass");						
+						}
+        			break;
+        			
+        			case 5:
+        			    if(!this.isExist(this.items.keys, "connection")){
+							this.add("connection");							
+						}else{
+							this.clickedItemShow("connection");						
+						}
+        			break;
+        			
+        			case 6:
+        			    if(!this.isExist(this.items.keys, "contacts")){
+							this.add("contacts");							
+						}else{
+							this.clickedItemShow("contacts");						
+						}        			
+        			break;
+        			
+        			case 7:
+        			    if(!this.isExist(this.items.keys, "device")){
+							this.add("device");							
+						}else{
+							this.clickedItemShow("device");						
+						}         			
+        			break;
+        			
+        			case 8:
+        			    if(!this.isExist(this.items.keys, "events")){
+							this.add("events");							
+						}else{
+							this.clickedItemShow("events");						
+						}         			
+        			break;
+        			
+        			case 9:
+        			    if(!this.isExist(this.items.keys, "file")){
+							this.add("file");							
+						}else{
+							this.clickedItemShow("file");						
+						}         			
+        			break;
+        			
+        			case 10:
+        			    if(!this.isExist(this.items.keys, "geolocation")){
+							this.add("geolocation");							
+						}else{
+							this.clickedItemShow("geolocation");						
+						}         			
+        			break;
+        			
+        			case 11:
+        			    if(!this.isExist(this.items.keys, "media")){
+							this.add("media");							
+						}else{
+							this.clickedItemShow("media");						
+						}         			
+        			break;
+        			
+        			case 12:
+        			    if(!this.isExist(this.items.keys, "notification")){
+							this.add("notification");							
+						}else{
+							this.clickedItemShow("notification");						
+						}          			
+        			break;
+        			
+        			case 13:
+        			    if(!this.isExist(this.items.keys, "storage")){
+							this.add("storage");							
+						}else{
+							this.clickedItemShow("storage");						
+						}         			
+        			break;
+        			
+        			default:        			
+        			break;
+        		}
+        	    this.doLayout();
+            },
         });
-        
+                        
         App.views.phonegapMain = new Ext.Panel({
         	id:'phonegapMain',
         	layout:'fit',
